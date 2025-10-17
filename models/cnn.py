96f739dfee470236b02ad1d7b263a25c03db37df
Lucia <lucianuovo7@gmail.com>
2025-10-17 16:46:56 +0200

    modello_training

---
+++ models/cnn.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+
+class CNN(nn.module):
+    def __init__(self,dropout: float = 0.0,use_batchnorm:bool=False,
+                 in_channels:int=1,num_classes:int=10):
+        super().__init__()
+
+        c1,c2 = 5,10
+        layers=[]
+        layers += [nn.Conv2d(in_channels,c1,kernel_size=3,padding=1),
+                   nn.ReLU(inplace=True)]
+        if use_batchnorm:
+            layers += [nn.BatchNorm2d(c1)]
+        layers += [nn.MaxPool2d(2,2)]
+        layers += [nn.Conv2d(c1,c2,kernel_size=3,padding=1),
+                   nn.ReLU(inplace=True)]
+        if use_batchnorm:
+            layers += [nn.BatchNorm2d(c2)]
+        layers += [nn.MaxPool2d(2,2)]
+        self.features = nn.Sequential(*layers)
+        self.dropout = nn.Dropout(dropout) if dropout > 0 else nn.Identity()
+        self.classifier = nn.Linear(7*7*c2,num_classes)
+
+    def forward(self,x:torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = torch.flatten(x,1)
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+
